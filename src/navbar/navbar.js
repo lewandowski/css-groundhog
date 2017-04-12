a3232811d43efeebeeea6ab75e25876f1f0081bc
Freihofner, Katrin <Katrin.Freihofner@dynatrace.com>
2017-04-12 15:37:44 +0200

    updated js according to toms feedback + using fetch for json results

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,46 +1,48 @@
 import $ from '../js-common-components/dollar';
 import { debounce } from '../js-common-components/utils';
 
-function fetchData(select) {
-  const rq = { url: '/assets/data/search_results.json',
-    onLoad: (e) => {
-      console.log('fetch data');
-      const options = (JSON.parse(e.target.responseText)).options;
-      if (options.length > 0) {
-        for (let i = 0; i < options.length; i++) {
-          const opt = document.createElement('li');
-          opt.value = options[i].value;
-          opt.innerHTML = options[i].value;
-          select.appendChild(opt);
-        }
-        select.classList.remove('expandable');
-        select.classList.add('is-expanded');
-      }
-    },
-  };
-
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', rq.onLoad.bind(this));
-
-  xhr.open('GET', rq.url);
-  xhr.send();
+function clearResults(select) {
+  const emptySearchResults = select;
+  emptySearchResults.classList.remove('is-expanded');
+  while (select.firstChild) {
+    select.removeChild(select.firstChild);
+  }
 }
 
+function createListElement(result) {
+  const opt = document.createElement('li');
+  const link = document.createElement('a');
+  link.setAttribute('href', result.url);
+  link.textContent = result.label;
+  opt.appendChild(link);
+  return opt;
+}
+
+function fetchResults(select) {
+  fetch('/assets/data/search_results.json')
+    .catch(() => '{ results: [] }')
+    .then(res => res.json())
+    .then((searchResults) => {
+      const results = searchResults.results;
+      const elements = results.map(createListElement);
+      window.requestAnimationFrame(() => {
+        clearResults(select);
+        select.classList.add('expandable', 'is-expanded');
+        elements
+          .forEach((el) => select.appendChild(el));
+      });
+    });
+}
 
 const initData = () => {
-  $('input').forEach(el => {
+  $('.js-search').forEach(el => {
     const ul = el.parentNode.appendChild(document.createElement('ul'));
-    ul.classList.add('search__results');
-    ul.classList.add('expandable');
-    el.addEventListener('keyup', debounce((ev) => {
-      ul.innerHTML = '';
-      console.log(ev);
-      fetchData(ul);
-    }, 250));
-    el.addEventListener('blur', (e) => {
-      console.log(e);
-      ul.innerHTML = '';
-      ul.classList.remove('is-expanded');
+    ul.classList.add('search__results', 'expandable');
+    el.addEventListener('keyup', debounce(() => {
+      fetchResults(ul);
+    }, 150));
+    el.addEventListener('blur', () => {
+      clearResults(ul);
     });
   });
 };
